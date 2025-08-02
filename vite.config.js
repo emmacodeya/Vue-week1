ad5faef5adad6f8c72b741e4a2b0c7ef07260d69
emma <kkes60128angel@gmail.com>
2025-08-03 01:37:11 +0800

    更新ghpages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Vue-week1/',
   plugins: [
     vue(),
     // vueDevTools(),
